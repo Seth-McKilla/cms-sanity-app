19a145a04f9fbadc84594c5776a93a3e7ba3830a
Seth McCullough <seth@endevrs.dev>
2022-07-31 10:26:49 -0400

    Add middleware function

---
+++ middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/", "/([^/.]*)"],
+};
+
+export default function middleware(req) {
+  const isProduction =
+    process.env.NODE_ENV === "production" && process.env.VERCEL === "1";
+
+  const url = req.nextUrl;
+
+  const hostname = req.headers.get("host");
+
+  const currentHost = hostname.replace(
+    isProduction ? ".vercel.app" : ".localhost:3000",
+    ""
+  );
+
+  if (currentHost === "app") {
+    url.pathname = `/app${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  if (["localhost:3000"].includes(hostname)) {
+    url.pathname = `/home${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+}
