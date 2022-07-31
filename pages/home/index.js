8ab4d73248f53fdcae2e45b338472f90f8aa4e0e
Seth McCullough <seth@endevrs.dev>
2022-07-31 10:30:47 -0400

    Separate app into home and app directories

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,16 +1,16 @@
-import Head from 'next/head'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { CMS_NAME } from '../lib/constants'
-import { indexQuery } from '../lib/queries'
-import { getClient, overlayDrafts } from '../lib/sanity.server'
+import Head from "next/head";
+import Container from "../../components/container";
+import MoreStories from "../../components/more-stories";
+import HeroPost from "../../components/hero-post";
+import Intro from "../../components/intro";
+import Layout from "../../components/layout";
+import { CMS_NAME } from "../../lib/constants";
+import { indexQuery } from "../../lib/queries";
+import { getClient, overlayDrafts } from "../../lib/sanity.server";
 
 export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout preview={preview}>
@@ -33,12 +33,12 @@ export default function Index({ allPosts, preview }) {
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery))
+  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery));
   return {
     props: { allPosts, preview },
-  }
+  };
 }
--- pages/home/posts/[slug].js
+++ pages/home/posts/[slug].js
@@ -1,33 +1,37 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Container from '../../components/container'
-import PostBody from '../../components/post-body'
-import MoreStories from '../../components/more-stories'
-import Header from '../../components/header'
-import PostHeader from '../../components/post-header'
-import SectionSeparator from '../../components/section-separator'
-import Layout from '../../components/layout'
-import PostTitle from '../../components/post-title'
-import { CMS_NAME } from '../../lib/constants'
-import { postQuery, postSlugsQuery } from '../../lib/queries'
-import { urlForImage, usePreviewSubscription } from '../../lib/sanity'
-import { sanityClient, getClient, overlayDrafts } from '../../lib/sanity.server'
+import Head from "next/head";
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import Container from "../../../components/container";
+import PostBody from "../../../components/post-body";
+import MoreStories from "../../../components/more-stories";
+import Header from "../../../components/header";
+import PostHeader from "../../../components/post-header";
+import SectionSeparator from "../../../components/section-separator";
+import Layout from "../../../components/layout";
+import PostTitle from "../../../components/post-title";
+import { CMS_NAME } from "../../../lib/constants";
+import { postQuery, postSlugsQuery } from "../../../lib/queries";
+import { urlForImage, usePreviewSubscription } from "../../../lib/sanity";
+import {
+  sanityClient,
+  getClient,
+  overlayDrafts,
+} from "../../../lib/sanity.server";
 
 export default function Post({ data = {}, preview }) {
-  const router = useRouter()
+  const router = useRouter();
 
-  const slug = data?.post?.slug
+  const slug = data?.post?.slug;
   const {
     data: { post, morePosts },
   } = usePreviewSubscription(postQuery, {
     params: { slug },
     initialData: data,
     enabled: preview && slug,
-  })
+  });
 
   if (!router.isFallback && !slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
 
   return (
@@ -50,7 +54,7 @@ export default function Post({ data = {}, preview }) {
                     content={urlForImage(post.coverImage)
                       .width(1200)
                       .height(627)
-                      .fit('crop')
+                      .fit("crop")
                       .url()}
                   />
                 )}
@@ -69,13 +73,13 @@ export default function Post({ data = {}, preview }) {
         )}
       </Container>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps({ params, preview = false }) {
   const { post, morePosts } = await getClient(preview).fetch(postQuery, {
     slug: params.slug,
-  })
+  });
 
   return {
     props: {
@@ -85,13 +89,13 @@ export async function getStaticProps({ params, preview = false }) {
         morePosts: overlayDrafts(morePosts),
       },
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const paths = await sanityClient.fetch(postSlugsQuery)
+  const paths = await sanityClient.fetch(postSlugsQuery);
   return {
     paths: paths.map((slug) => ({ params: { slug } })),
     fallback: true,
-  }
+  };
 }
